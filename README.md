791e6450b3460bb2793593b57a27ef03da7db71a
Alessandro Carrano <alessandro.carrano@gmail.com>
2022-12-21 14:35:12 +0100

    update

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # test_terraform 5
+
+docker build -t terraformimage .
+docker run -e AWS_ACCESS_KEY_ID="<YOUR_ACCESS_KEY>" -e AWS_SECRET_ACCESS_KEY="<YOUR_ACCESS_KEY>" terraformimage -c "terraform init;terraform plan"  
--- main.tf
+++ main.tf
@@ -5,10 +5,6 @@ provider "aws" {
  terraform {
    backend "s3" {
      key            = "global/s3/terraform.tfstate"
-     bucket         = "alessandro-test-s3"
-     region         = "us-east-2"
-     dynamodb_table = "alessandro-test-dynamo"
-     encrypt        = false
    }
  }
 
